bd53a2e9d50b8ca6930d63c0cffe5cff3a2dda3e
Jillian Anderson <jillianderson8@gmail.com>
2017-01-19 22:48:03 -0500

    Begin tracking all current files

---
+++ cydr/R/PassTurnFilter.R
@@ -0,0 +1,68 @@
+# **************************************************************************** #
+# Copyright (C) 2016 Jillian Anderson                                          #
+# This file is part of the cydr package developed by Jillian Anderson during   #
+# her 4th year Knowledge Integration Honours thesis at the University of       #
+# Waterloo.                                                                    #
+#                                                                              #
+# cydr is free software: you can redistribute it and/or modify it under the    #
+# terms of a GNU General Public License as published by the Free Software      #
+# Foundation. cydr is distributed in the hope that it will be useful, but      #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   #
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for #
+# more details.                                                                #
+#                                                                              #
+# You should have received a copy of the GNU General Public License along with #
+# cydr. If not, see <http://www.gnu.org/licenses/>.                            #
+# **************************************************************************** #
+
+#' A helper function for ____ which calculates difference between two angles.
+#'
+#' @param oldDir a number in the range of \[0, 360\)
+#' @param newDir a number in the range of \[0, 360\)
+#' @return The difference between two angles.
+#' @examples
+#' calc_direction_change(20, 180)
+#' calc_direction_change(350, 1)
+#' calc_direction_change(0, 180)
+#'
+calc_direction_change <- function(oldDir, newDir){
+  diff <- newDir - oldDir
+  if (is.na(diff)){
+    return(NA_real_)
+  }else if (diff < -180){
+    diff = diff + 360
+  } else if (diff > 180){
+    diff = diff - 360
+  }
+  return(diff)
+}
+
+#' Identifies turns.
+#' @description id_pass_turns identifies turns at the end of passes within a
+#' field.
+#' @param data a dataframe
+#' @param remove a Boolean. Defaults to FALSE.
+#' @return A dataframe. Added column called cydr_Error. If a potential error
+#' has been identified this column is TRUE. Otherwise it is FALSE.
+#' @examples
+#' id_pass_turns(field1)
+#' id_pass_turns(field1, remove=TRUE)
+id_pass_turns <- function(data, remove=FALSE){
+  data_errors <- data %>%
+    group_by(Dataset) %>%
+    arrange(Obj__Id) %>%
+    mutate(SOldDir = lag(Track_deg, n = 5)) %>%
+    mutate(SFutDir = lead(Track_deg, n = 5)) %>%
+    mutate(LOldDir = lag(Track_deg, n = 20)) %>%
+    mutate(LFutDir = lead(Track_deg, n = 20)) %>%
+    rowwise() %>%
+    mutate(ShortChange = calc_direction_change(SOldDir, SFutDir)) %>%
+    mutate(LongChange = calc_direction_change(LOldDir, SFutDir)) %>%
+    mutate(cydr_Error = !(abs(ShortChange)<45 || abs(LongChange)<100))
+
+  if(remove)
+    data_errors <- data %>%
+      filter(is.na(cydr_Error) | !cydr_Error)
+
+  return(data)
+}
