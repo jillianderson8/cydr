474f6736331d0af9d92991dad63b8db2f62fa638
Jillian Anderson <jillianderson8@gmail.com>
2017-01-19 22:55:07 -0500

    Added file headers

---
+++ cydr/R/NarrowPassesFilter.R
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+# Copyright (C) 2016 Jillian Anderson                                          #
+# This file is part of the cydr package developed by Jillian Anderson during   #
+# her 4th year Knowledge Integration Honours thesis at the University of       #
+# Waterloo.                                                                    #
+#                                                                              #
+# cydr is free software: you can redistribute it and/or modify it under the    #
+# terms of a GNU General Public License as published by the Free Software      #
+# Foundation. cydr is distributed in the hope that it will be useful, but      #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   #
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for #
+# more details.                                                                #
+#                                                                              #
+# You should have received a copy of the GNU General Public License along with #
+# cydr. If not, see <http://www.gnu.org/licenses/>.                            #
+# **************************************************************************** #
